976adb0415d9e9a5da25f3922eaddcb7181f174a
Jesus Izquierdo <jizquierdo@contactel.es>
2021-05-14 13:16:20 +0100

    primer commit

---
+++ resolvers/posts.js
@@ -0,0 +1,8 @@
+export default {
+  Query: {
+    getPost: (parent, args, {models}) => models.Post.findOne(args)
+  },
+  Mutation: {
+    createPost: (parent, args, {models}) => models.Post.create(args.post)
+  }
+}
